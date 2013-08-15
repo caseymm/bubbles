5e5687b637c8b131537b5a4a92f4b6e7117ffd5f
caseymm <caseymm@gmail.cpom>
2013-08-15 11:49:10 -0400

    added 300px version for phone to show and hide depending on px width/for mobile

---
+++ js/barSmall.js
@@ -0,0 +1,101 @@
+function bar() {
+
+
+var data;
+
+var margin = {top: 20, right: 0, bottom: 130, left: -3},
+    width = 300 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+var commasFormatter = d3.format(",.0f");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .2);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(commasFormatter);
+
+var svg = d3.select("#bar").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("data/hopeNums.json", function(json) {
+  data = json;
+  x.domain(data.map(function(d) { return d.abbr; }));
+  y.domain([0, d3.max(data, function(d) { return +d.students; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")  
+            .style("text-anchor", "end")
+            .style("font-size", "11px")
+            .attr("dx", "-.4em")
+            .attr("dy", "-.5em")
+            .attr("transform", function(d) {
+                return "rotate(-90)" 
+                });
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end");
+      //.text("Students");
+
+d3.select("#addBar").on("click", function(e){
+  svg.selectAll(".bar")
+      .data(json)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("id", function(d) { return (d.id)+'bar'; })
+      .attr("x", function(d) { return x(d.abbr); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.students); })
+      .attr("height", function(d) { return height - y(d.students); })
+      .call(d3.helper.tooltip()
+            .text(function(d){ return 'School: '+ d.college + '<br />Type of School: '+ d.type +'<br />HOPE Students: ' +commasFormatter(d.students) +'<br />Amount Awarded: $'+ commasFormatter(d.amount); })
+        )
+      .on('mouseover', function(d){
+          d3.select(this).transition().duration(200).style({opacity:'0.8', fill: d.fill})
+          $('#'+d.id+'ball').css( {"opacity": "0.8", "transition-duration": "300ms"} )
+          $('#'+d.id+'text').css( {"opacity": "1", "transition-duration": "300ms"} );
+                    })
+      .on('mouseout', function(d){
+          d3.select(this).transition().duration(200).style({opacity:'1', fill:"#C2C2C2"})
+          $('#'+d.id+'ball').css( {"opacity": "0.0", "transition-duration": "200ms"})
+          $('#'+d.id+'text').css( {"opacity": "0.0", "transition-duration": "200ms"});
+                    });
+   
+   svg.selectAll("rect")
+      .attr("transform", "translate(850,0)")
+      .transition()
+      .delay(function (d){ return d.lineup * 300;})
+      .duration(2000)
+      .attr("transform", "translate(0,0)")
+      .each("end", function() { d3.select(this).attr("transform", "translate(0,0)"); });
+
+});  
+     
+});
+
+function type(d) {
+  d.students = +d.students;
+  return d;
+}
+};
---
+++ js/bubblesSmall.js
@@ -0,0 +1,194 @@
+$(document).ready(function() {
+  $('#typeInfo').hide();
+  $('#mostToLeast').hide();
+  $('#bar').hide();
+  
+  bubblesSmall();
+  });
+
+function bubblesSmall() {
+var w = 300,
+    h = 300;
+
+var svg = d3.select("#bubbles").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+
+
+var commasFormatter = d3.format(",.0f");
+var radius_scale = d3.scale.pow().exponent(0.5).domain([0, 107848]).range([2, 40]);
+
+d3.json("data/hopeChildren.json", function(json) {
+  var force = d3.layout.force()
+    .gravity(0.05)
+    .charge(function(d, i) { return d.students*-0.003; })
+    .nodes(json.children)
+    .size([w, h])
+    .links([])
+    .start();
+    
+      
+
+var node = svg.selectAll(".node")
+        .data(json.children)
+        .enter().append("svg:g")
+        .attr("class", "node")
+        .on('mouseover', function(d){
+            var thisNode = d3.select(this);
+            thisNode.select("text").style({opacity:'1.0'});
+                    })
+        .on('mouseout', function(d){
+            var thisNode = d3.select(this);
+            thisNode.select("text").style({opacity:'0.0'});
+                    });
+
+node.append("image")
+      .attr("xlink:href", (function(d) { return d.img; }))
+      .attr("x", function(d) { return (radius_scale(parseInt(d.students))*-1); })
+      .attr("y", function(d) { return (radius_scale(parseInt(d.students))*-1); })
+      .attr("width", function(d) { return (radius_scale(parseInt(d.students))*2); })
+      .attr("height", function(d) { return (radius_scale(parseInt(d.students))*2); });     
+      
+node.append("svg:circle")
+        //.attr("cx", function(d) { return d.x; })
+        //.attr("cy", function(d) { return d.y; })
+        .attr("title", function(d) { return d.colleges; })
+        .attr("r", function(d) { return radius_scale(parseFloat(d.students)); })
+        .attr("fill", function(d) { return d.fill; })
+        .attr("stroke", function(d) { return d.stroke; })
+        //.attr("id", (function(d) { return d.id+"ball"; }))
+        .call(d3.helper.tooltip()
+            .text(function(d){ return 'School: '+ d.college + '<br />Type of School: '+ d.type +'<br />HOPE Students: ' +commasFormatter(d.students) +'<br />Amount Awarded: $'+ commasFormatter(d.amount); })
+        )
+        .call(force.drag);        
+
+node.append("text")
+      .attr("id", (function(d) { return d.id+"text"; }))
+      .attr("dx", 0)
+      //.attr("dy", function(d) { return (radius_scale(parseInt(d.students))*.7); })
+      .attr("dy", function(d) { return (radius_scale(parseInt(d.students))*.2); })
+      .attr("text-anchor", "middle")
+      .style("font-size", function(d) { return (radius_scale(parseFloat(d.students))*.4)+"px"; })
+      .text(function(d) { return commasFormatter(d.students); });
+
+svg.style("opacity", 1e-6)
+  .transition()
+    .duration(1000)
+    .style("opacity", 1);
+    
+      
+force.on("tick", function(e) {
+      svg.selectAll("circle").attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")";})
+         .on('mouseover', function(d){
+          d3.select(this).transition().duration(300).style({opacity:'0.8'});
+                    })
+         .on('mouseout', function(d){
+          d3.select(this).transition().duration(200).style({opacity:'0.0',});
+                    });
+      svg.selectAll("image").attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+      svg.selectAll("text").attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+    });
+
+d3.select("#sort").on("click", function(e){
+    d3.select(".selected").classed("selected", false)
+            d3.select(this).classed("selected", true)
+  $('#typeInfo').show()
+  $('#mostToLeast').hide()
+  $('#bar').hide()
+  force.on("tick", function(e) {
+    svg.selectAll("circle").attr("r", function(d) { return radius_scale(parseInt(d.students));})
+       .attr("id", (function(d) { return d.id+""; }))
+       .attr("transform", function(d) { return "translate(" + (d.xValue/2.7) + "," + d.yValue/2.1 + ")";})
+       .on('mouseover', function(d){
+          d3.select(this).transition().duration(300).style({opacity:'0.8'});
+                    })
+       .on('mouseout', function(d){
+          d3.select(this).transition().duration(200).style({opacity:'0.0',});
+                    });
+    svg.selectAll("image")
+      .transition().duration(100)
+      .attr("x", function(d) { return (radius_scale(parseInt(d.students))*-1); })
+      .attr("y", function(d) { return (radius_scale(parseInt(d.students))*-1); })
+      .attr("width", function(d) { return (radius_scale(parseInt(d.students))*2); })
+      .attr("height", function(d) { return (radius_scale(parseInt(d.students))*2); })
+      .attr("transform", function(d) { return "translate(" + (d.xValue/2.7) + "," + d.yValue/2.1 + ")";});
+    svg.selectAll("text")
+      .style("font-size", function(d) { return (radius_scale(parseInt(d.students))*.4)+"px";})
+      .attr("dy", function(d) { return (radius_scale(parseInt(d.students))*.2); })
+      .attr("transform", function(d) { return "translate(" + (d.xValue/2.7) + "," + d.yValue/2.1 + ")";});
+  force.resume();
+  });
+});
+d3.select("#sortSize").on("click", function(e){
+    d3.select(".selected").classed("selected", false)
+            d3.select(this).classed("selected", true)
+  $('#typeInfo').hide()
+  $('#mostToLeast').show()
+  $('#bar').show()
+  force.on("tick", function(e) {
+    svg.selectAll("circle").attr("r", function(d) { return radius_scale(parseInt(d.students)*.25); })
+      .attr("transform", function(d) { return "translate(" + ((d.xValueB-70)/2.65) + "," + d.yValueB/2.1 + ")";})
+      .attr("id", (function(d) { return d.id+"ball"; }))
+      .on('mouseover', function(d){
+          d3.select(this).transition().duration(200).style({opacity:'0.8'})
+          $('#'+d.id+'bar').css( {"fill": ''+d.fill+'', "transition-duration": "200ms"} );
+                    })
+      .on('mouseout', function(d){
+          d3.select(this).transition().duration(200).style({opacity:'0.0',})
+          $('#'+d.id+'bar').css( {"fill": "#C2C2C2", "transition-duration": "200ms"} );
+                    });
+    svg.selectAll("image")
+      .transition().duration(100)
+      .attr("x", function(d) { return (radius_scale(parseInt(d.students))*-.5); })
+      .attr("y", function(d) { return (radius_scale(parseInt(d.students))*-.5); })
+      .attr("width", function(d) { return (radius_scale(parseInt(d.students))); })
+      .attr("height", function(d) { return (radius_scale(parseInt(d.students))); })
+      .attr("transform", function(d) { return "translate(" + ((d.xValueB-70)/2.65) + "," + d.yValueB/2.1 + ")";});
+    svg.selectAll("text")
+      .style("font-size", function(d) { return (radius_scale(parseFloat(d.students))*.2)+"px";})
+      .attr("dy", function(d) { return (radius_scale(parseInt(d.students))*.1); })
+      .attr("transform", function(d) { return "translate(" + ((d.xValueB-70)/2.65) + "," + d.yValueB/2.1 + ")";});
+  force.resume();
+  });
+});
+  
+d3.select("#unSort").on("click", function(e){
+    d3.select(".selected").classed("selected", false)
+            d3.select(this).classed("selected", true)
+  $('#typeInfo').hide()
+  $('#mostToLeast').hide()
+  $('#bar').hide()
+  force.on("tick", function(e) {
+    svg.selectAll("circle")
+       .attr("id", (function(d) { return d.id+""; }))
+       .attr("r", function(d) { return radius_scale(parseInt(d.students));})
+       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";})
+       .on('mouseover', function(d){
+          d3.select(this).transition().duration(300).style({opacity:'0.8'});
+                    })
+       .on('mouseout', function(d){
+          d3.select(this).transition().duration(200).style({opacity:'0.0',});
+                    });
+       
+    svg.selectAll("image").attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";})
+      .transition().duration(150)
+      .attr("x", function(d) { return (radius_scale(parseInt(d.students))*-1); })
+      .attr("y", function(d) { return (radius_scale(parseInt(d.students))*-1); })
+      .attr("width", function(d) { return (radius_scale(parseInt(d.students))*2); })
+      .attr("height", function(d) { return (radius_scale(parseInt(d.students))*2); })
+      
+      .each("end", function() { d3.select(this).transition().duration(0); });
+   svg.selectAll("text")
+      .style("font-size", function(d) { return (radius_scale(parseInt(d.students))*.4)+"px";})
+      .attr("dy", function(d) { return (radius_scale(parseInt(d.students))*.2); })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";});
+  force.resume();
+  });
+ });
+});
+bar ();
+};
